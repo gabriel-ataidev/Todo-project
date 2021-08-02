0aaa51132b7a47ab8b2fc8d0f12be13e510fb14a
Gabriel Ataide <gabriel.ataide07@gmail.com>
2021-08-02 19:04:38 -0300

    giving some funcionalities

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const todoList = document.querySelector('.todo-list');
 
 //Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
 
 //Functions
 function addTodo(event){
@@ -32,4 +33,17 @@ function addTodo(event){
     todoList.appendChild(todoDiv);
     //clear todoInput value
     todoInput.value = "";
+}
+function deleteCheck(e){
+    const item = e.target;
+    //delete todo
+    if(item.classList[0] === 'trash-btn'){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+    //check mark todo
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
 }
